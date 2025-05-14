298915b635e2ea618386ed3222321db3688088b8
Tim <tha111@sfu.ca>
2025-05-13 21:22:28 -0400

    Compute absolute and relative emulator error

--- analysis/synthetic/eval_gp.py
+++ analysis/synthetic/eval_gp.py
@@ -66,6 +66,114 @@ def eval_emulator(train_config, test_config, bh_config, m, p):
     return
 
 
+def eval_emulator_abs(train_config, test_config, bh_config, m, p):
+    train_config = tools.import_config(train_config)
+    test_config = tools.import_config(test_config)
+
+    ypred = np.load('data/pred/y_test_pred_m{:03d}_p{:02d}.npy'.format(m,p))
+    ypred_mean = np.mean(ypred, axis=0)
+
+    print(test_config.Y_physical)
+    ytest = np.load(test_config.Y_physical, mmap_mode='r').T
+
+    bh_config = np.load(bh_config, allow_pickle=True)
+    print(bh_config)
+    yind = bh_config['node']*365 + np.arange(365)
+    ytest = ytest[:, yind]
+    err = ypred_mean - ytest
+    rmse = np.sqrt(np.mean(err**2, axis=-1))
+    ix = np.argsort(rmse)
+    nums = np.array([5, 50, 95])[::-1]
+    sim_nums = ix[nums-1]
+
+    alphabet = ['(a)', '(b)', '(c)']
+    fig,axs = plt.subplots(figsize=(3.5, 4), sharex=True, nrows=3)
+    for i in range(3):
+        ax = axs[i]
+        yi = ytest[sim_nums[i]]
+        # ax.plot(ytest[sim_nums[i]], color='k', label='GlaDS')
+        ax.plot(ypred_mean[sim_nums[i]]-yi, color='r', label='Emulator error')
+        qq = np.quantile(ypred[:, sim_nums[i], :]-yi, np.array([0.025, 0.16, 0.84, 0.975]), axis=0)
+        ax.fill_between(np.arange(365), qq[0], qq[-1], color='r',
+            alpha=0.3, edgecolor='none', label='Emulator error 95% interval')
+        ax.fill_between(np.arange(365), qq[1], qq[2], color='firebrick',
+            alpha=0.5, edgecolor='none', label='Emulator error 68% interval')
+        ax.grid(linewidth=0.5)
+        ax.set_xlim([125, 300])
+        ax.spines[['top', 'right']].set_visible(False)
+        ax.text(0.025, 0.9, alphabet[i], transform=ax.transAxes,
+            fontweight='bold', ha='left', va='top')
+        
+        ax.text(0.975, 0.9, 'RMSE = {:.3f}'.format(rmse[sim_nums[i]]),
+            ha='right', va='top', transform=ax.transAxes)
+        ylim = ax.get_ylim()
+        ax.set_ylim([-np.max(ylim), np.max(ylim)])
+    
+    axs[0].legend(bbox_to_anchor=(0,1,1,0.3), ncols=1, loc='lower left',
+        frameon=False)
+    
+    axs[-1].set_xlabel('Day of 2012')
+    axs[1].set_ylabel('Flotation fraction')
+    fig.subplots_adjust(left=0.125, bottom=0.1, top=0.85, right=0.95,
+        hspace=0.15)
+    fig.savefig('figures/gp_timeseries_error_abs.png', dpi=400)
+
+    return
+
+def eval_emulator_rel(train_config, test_config, bh_config, m, p):
+    train_config = tools.import_config(train_config)
+    test_config = tools.import_config(test_config)
+
+    ypred = np.load('data/pred/y_test_pred_m{:03d}_p{:02d}.npy'.format(m,p))
+    ypred_mean = np.mean(ypred, axis=0)
+
+    print(test_config.Y_physical)
+    ytest = np.load(test_config.Y_physical, mmap_mode='r').T
+
+    bh_config = np.load(bh_config, allow_pickle=True)
+    print(bh_config)
+    yind = bh_config['node']*365 + np.arange(365)
+    ytest = ytest[:, yind]
+    err = ypred_mean - ytest
+    rmse = np.sqrt(np.mean(err**2, axis=-1))
+    ix = np.argsort(rmse)
+    nums = np.array([5, 50, 95])[::-1]
+    sim_nums = ix[nums-1]
+
+    alphabet = ['(a)', '(b)', '(c)']
+    fig,axs = plt.subplots(figsize=(3.5, 4), sharex=True, nrows=3)
+    for i in range(3):
+        ax = axs[i]
+        yi = ytest[sim_nums[i]]
+        # ax.plot(ytest[sim_nums[i]], color='k', label='GlaDS')
+        ax.plot((ypred_mean[sim_nums[i]]-yi)/yi, color='r', label='Emulator error')
+        qq = np.quantile((ypred[:, sim_nums[i], :]-yi)/yi, np.array([0.025, 0.16, 0.84, 0.975]), axis=0)
+        ax.fill_between(np.arange(365), qq[0], qq[-1], color='r',
+            alpha=0.3, edgecolor='none', label='Emulator error 95% interval')
+        ax.fill_between(np.arange(365), qq[1], qq[2], color='firebrick',
+            alpha=0.5, edgecolor='none', label='Emulator error 68% interval')
+        ax.grid(linewidth=0.5)
+        ax.set_xlim([125, 300])
+        ax.spines[['top', 'right']].set_visible(False)
+        ax.text(0.025, 0.9, alphabet[i], transform=ax.transAxes,
+            fontweight='bold', ha='left', va='top')
+        
+        ax.text(0.975, 0.9, 'RMSE = {:.3f}'.format(rmse[sim_nums[i]]),
+            ha='right', va='top', transform=ax.transAxes)
+        ylim = ax.get_ylim()
+        ax.set_ylim([-np.max(ylim), np.max(ylim)])
+    
+    axs[0].legend(bbox_to_anchor=(0,1,1,0.3), ncols=1, loc='lower left',
+        frameon=False)
+    
+    axs[-1].set_xlabel('Day of 2012')
+    axs[1].set_ylabel('Flotation fraction')
+    fig.subplots_adjust(left=0.125, bottom=0.1, top=0.85, right=0.95,
+        hspace=0.15)
+    fig.savefig('figures/gp_timeseries_error_rel.png', dpi=400)
+
+    return
+
 def eval_all_emulators(train_config, test_config, bh_config,
     table='table.dat'):
     train_config = tools.import_config(train_config)
@@ -166,5 +274,9 @@ if __name__=='__main__':
     args = parser.parse_args()
 
     eval_emulator(args.train_config, args.test_config, args.bh_config,
+        m=args.m, p=args.p)
+    eval_emulator_abs(args.train_config, args.test_config, args.bh_config,
+        m=args.m, p=args.p)
+    eval_emulator_rel(args.train_config, args.test_config, args.bh_config,
         m=args.m, p=args.p)
     eval_all_emulators(args.train_config, args.test_config, args.bh_config)
